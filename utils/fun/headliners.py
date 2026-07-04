69e73ffb6e7077ef2c1b76a4bbaba582ca53c79b
oxygen-me <bradleyjensen2020@gmail.com>
2026-07-03 19:07:17 -0700

    created headliners.py and added some content

---
+++ utils/fun/headliners.py
@@ -0,0 +1,11 @@
+
+# --- window names
+window_names = [
+    "Sephiwindow",
+    "Left Shoe",
+    "Fortnite",
+    "Estuans Interius",
+    "Ira Vehementi",
+    "Buckets of Piss",
+    "Don has Crabs"
+ ]
