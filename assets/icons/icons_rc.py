e85a41f1e9dcc5c1822209016744c4145fdd8aba
oxygen-me <bradleyjensen2020@gmail.com>
2026-07-04 18:06:28 -0700

    embed icons

---
+++ assets/icons/icons_rc.py
@@ -0,0 +1,212 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.11.1
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x03x\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2224\
+\x22 height=\x2224\x22 vi\
+ewBox=\x220 0 24 24\
+\x22 fill=\x22none\x22 st\
+roke=\x22currentCol\
+or\x22 stroke-width\
+=\x222\x22 stroke-line\
+cap=\x22round\x22 stro\
+ke-linejoin=\x22rou\
+nd\x22 class=\x22icon \
+icon-tabler icon\
+s-tabler-outline\
+ icon-tabler-set\
+tings\x22><path str\
+oke=\x22none\x22 d=\x22M0\
+ 0h24v24H0z\x22 fil\
+l=\x22none\x22 /><path\
+ d=\x22M10.325 4.31\
+7c.426 -1.756 2.\
+924 -1.756 3.35 \
+0a1.724 1.724 0 \
+0 0 2.573 1.066c\
+1.543 -.94 3.31 \
+.826 2.37 2.37a1\
+.724 1.724 0 0 0\
+ 1.065 2.572c1.7\
+56 .426 1.756 2.\
+924 0 3.35a1.724\
+ 1.724 0 0 0 -1.\
+066 2.573c.94 1.\
+543 -.826 3.31 -\
+2.37 2.37a1.724 \
+1.724 0 0 0 -2.5\
+72 1.065c-.426 1\
+.756 -2.924 1.75\
+6 -3.35 0a1.724 \
+1.724 0 0 0 -2.5\
+73 -1.066c-1.543\
+ .94 -3.31 -.826\
+ -2.37 -2.37a1.7\
+24 1.724 0 0 0 -\
+1.065 -2.572c-1.\
+756 -.426 -1.756\
+ -2.924 0 -3.35a\
+1.724 1.724 0 0 \
+0 1.066 -2.573c-\
+.94 -1.543 .826 \
+-3.31 2.37 -2.37\
+c1 .608 2.296 .0\
+7 2.572 -1.065\x22 \
+/><path d=\x22M9 12\
+a3 3 0 1 0 6 0a3\
+ 3 0 0 0 -6 0\x22 /\
+></svg>\
+\x00\x00\x01c\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2224\
+\x22 height=\x2224\x22 vi\
+ewBox=\x220 0 24 24\
+\x22 fill=\x22none\x22 st\
+roke=\x22currentCol\
+or\x22 stroke-width\
+=\x222\x22 stroke-line\
+cap=\x22round\x22 stro\
+ke-linejoin=\x22rou\
+nd\x22 class=\x22icon \
+icon-tabler icon\
+s-tabler-outline\
+ icon-tabler-ter\
+minal\x22><path str\
+oke=\x22none\x22 d=\x22M0\
+ 0h24v24H0z\x22 fil\
+l=\x22none\x22 /><path\
+ d=\x22M5 7l5 5l-5 \
+5\x22 /><path d=\x22M1\
+2 19l7 0\x22 /></sv\
+g>\
+\x00\x00\x02\x9d\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2224\
+\x22 height=\x2224\x22 vi\
+ewBox=\x220 0 24 24\
+\x22 fill=\x22none\x22 st\
+roke=\x22currentCol\
+or\x22 stroke-width\
+=\x222\x22 stroke-line\
+cap=\x22round\x22 stro\
+ke-linejoin=\x22rou\
+nd\x22 class=\x22icon \
+icon-tabler icon\
+s-tabler-outline\
+ icon-tabler-lay\
+out-grid\x22><path \
+stroke=\x22none\x22 d=\
+\x22M0 0h24v24H0z\x22 \
+fill=\x22none\x22 /><p\
+ath d=\x22M4 5a1 1 \
+0 0 1 1 -1h4a1 1\
+ 0 0 1 1 1v4a1 1\
+ 0 0 1 -1 1h-4a1\
+ 1 0 0 1 -1 -1l0\
+ -4\x22 /><path d=\x22\
+M14 5a1 1 0 0 1 \
+1 -1h4a1 1 0 0 1\
+ 1 1v4a1 1 0 0 1\
+ -1 1h-4a1 1 0 0\
+ 1 -1 -1l0 -4\x22 /\
+><path d=\x22M4 15a\
+1 1 0 0 1 1 -1h4\
+a1 1 0 0 1 1 1v4\
+a1 1 0 0 1 -1 1h\
+-4a1 1 0 0 1 -1 \
+-1l0 -4\x22 /><path\
+ d=\x22M14 15a1 1 0\
+ 0 1 1 -1h4a1 1 \
+0 0 1 1 1v4a1 1 \
+0 0 1 -1 1h-4a1 \
+1 0 0 1 -1 -1l0 \
+-4\x22 /></svg>\
+\x00\x00\x01\xc0\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2224\
+\x22 height=\x2224\x22 vi\
+ewBox=\x220 0 24 24\
+\x22 fill=\x22none\x22 st\
+roke=\x22currentCol\
+or\x22 stroke-width\
+=\x222\x22 stroke-line\
+cap=\x22round\x22 stro\
+ke-linejoin=\x22rou\
+nd\x22 class=\x22icon \
+icon-tabler icon\
+s-tabler-outline\
+ icon-tabler-hom\
+e\x22><path stroke=\
+\x22none\x22 d=\x22M0 0h2\
+4v24H0z\x22 fill=\x22n\
+one\x22 /><path d=\x22\
+M5 12l-2 0l9 -9l\
+9 9l-2 0\x22 /><pat\
+h d=\x22M5 12v7a2 2\
+ 0 0 0 2 2h10a2 \
+2 0 0 0 2 -2v-7\x22\
+ /><path d=\x22M9 2\
+1v-6a2 2 0 0 1 2\
+ -2h2a2 2 0 0 1 \
+2 2v6\x22 /></svg>\
+"
+
+qt_resource_name = b"\
+\x00\x05\
+\x00o\xa6S\
+\x00i\
+\x00c\x00o\x00n\x00s\
+\x00\x0c\
+\x0b\xdf,\xc7\
+\x00s\
+\x00e\x00t\x00t\x00i\x00n\x00g\x00s\x00.\x00s\x00v\x00g\
+\x00\x07\
+\x0a,Z\x07\
+\x00c\
+\x00l\x00i\x00.\x00s\x00v\x00g\
+\x00\x08\
+\x07vW\xc7\
+\x00a\
+\x00p\x00p\x00s\x00.\x00s\x00v\x00g\
+\x00\x08\
+\x068W'\
+\x00h\
+\x00o\x00m\x00e\x00.\x00s\x00v\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x04\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00X\x00\x00\x00\x00\x00\x01\x00\x00\x07\x84\
+\x00\x00\x01\x9f/\xb3\xba\xa7\
+\x00\x00\x00B\x00\x00\x00\x00\x00\x01\x00\x00\x04\xe3\
+\x00\x00\x01\x9f/\xb4a\xfa\
+\x00\x00\x00.\x00\x00\x00\x00\x00\x01\x00\x00\x03|\
+\x00\x00\x01\x9f/\xb4\xfbD\
+\x00\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x9f/\xb4\x9f\xad\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
