bb6f9742503d48439442d995f905d15de44142dc
oxygen-me <bradleyjensen2020@gmail.com>
2026-07-11 19:16:22 -0700

    create fundamental scripts

---
+++ src/sephirothos/metadata.py
@@ -0,0 +1,9 @@
+"""Static application metadata."""
+
+APPLICATION_NAME = "SephirothOS"
+ORGANIZATION_NAME = "oxygen-me"
+VERSION = "0.1.0a1"
+RELEASE_CHANNEL = "alpha"
+
+REPOSITORY_URL = "https://github.com/oxygen-me/SephirothOS-v2"
+RELEASES_URL = f"{REPOSITORY_URL}/releases"
