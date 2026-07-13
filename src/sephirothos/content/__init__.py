f753410ee8b1bdc2b654e3d93efaecf9ce8af8ce
oxygen-me <bradleyjensen2020@gmail.com>
2026-07-13 01:11:09 -0700

    create content dir

---
+++ src/sephirothos/content/__init__.py
@@ -0,0 +1,14 @@
+"""Static textual content bundled with SephirothOS."""
+
+from sephirothos.content.quotes import QUOTES, Quote
+from sephirothos.content.subtitles import HOME_SUBTITLES
+from sephirothos.content.tips import TIPS
+from sephirothos.content.window_titles import WINDOW_TITLES
+
+__all__ = (
+    "HOME_SUBTITLES",
+    "QUOTES",
+    "TIPS",
+    "WINDOW_TITLES",
+    "Quote",
+)
